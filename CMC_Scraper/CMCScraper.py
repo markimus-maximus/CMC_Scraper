aa33ea1e33d4eaf8507c23a629fc6ad80c2794a7
Mark Owen <markowen90@hotmail.co.uk>
2022-10-06 13:26:18 +0100

    Code moved over to DH

--- CMC_Scraper/CMCScraper.py
+++ CMC_Scraper/CMCScraper.py
@@ -1,36 +1,26 @@
 from msilib.schema import Binary
 from bs4 import BeautifulSoup
-from datetime import date, datetime, timedelta
-import logging
 from botocore.exceptions import ClientError
 import numpy as np
 import os
 import time
-from os import listdir
-from os.path import isfile, join
 import pandas as pd
 from pathlib import Path
 import requests
 from selenium.webdriver.common.by import By
 import urllib.request
-from DataHandler import DataHandling
-import uuid
-import json
-import logging
+from DataHandler import DataHandler
 import re
-import boto3
-import psycopg2
 from sqlalchemy import create_engine
 from sqlalchemy import inspect
-import itertools
-import ntpath
+import DataHandler
 
 
 
 
 
 
-class CMCScraper:
+class CMCScraper(DataHandler.DataHandler):
     ''' Class for scraping data from Coin Market Cap, although much of this code could be easily adapted for other scraping applications.
     
     This class can generate a list of dates between 2 given dates and formats the list in order to  use them as permalinks, 
@@ -408,163 +398,7 @@ class CMCScraper:
         assert self.total_entries_appended == self.average_entries, "The number of total entries in the appended list does not match the total_crypto_url_tag_entries"
         return all_scraped_data_list 
 
-    @staticmethod
-    def create_crypto_dataframe(list_of_lists_for_table:list):
-        return pd.DataFrame.from_records(list_of_lists_for_table, columns=["ID", "source_url", "Rank", "Name", "Market Capitalisation", "Price", "Circulating Supply", "Ticker", "24 h change"])
     
-    @staticmethod
-    def save_dataframe_locally( dataframe, 
-                                path:str):
-        return dataframe.to_csv(path_or_buf=path)
-
-    @staticmethod
-    def csv_to_dataframe(csv_path:str):
-        return pd.read_csv(csv_path)
-        
-    @staticmethod
-    def UUID_dictionary(record_list:list):
-        ''' 
-        This method generates UUIDs for every record in the method attribute list and stores in a list, before concatenating the
-        UUID with a list to generate a dictionary.
-        
-        syntax: UUID_dictionary(record_list)
-        
-        Takes 1 argument.
-        record_list argument= the list of records which are to be concatenated to UUIDs
-          '''
-        uuid_list = []   
-        #generate a pseudo-unique uuid for every record in the list by taking the length of the list
-        for UUID in range(len(record_list)):
-            #generates 1 uuid4 and converts to string (this is easier for dictionary saving to JSON)
-            single_uuid = str(uuid.uuid4())
-            #append the UUID to the list
-            uuid_list.append(single_uuid)
-            
-        #making the dictionary with the record list and the uuid list just generated
-        record_uuid_dict = {}
-        record_uuid_dict = dict(zip(uuid_list, record_list))
-        return record_uuid_dict
-
-    @staticmethod
-    def turn_dictionary_into_json_file( path:str, 
-                                        file_to_turn_into_json:str):
-        '''This method converts a file to a JSON file and saves it to a specified path.
-        
-        syntax: turn_dictionary_into_json_file(path, dictionary_to_turn_into_json)
-
-        Takes 2 arguments
-        path argument = path for file to be written to and name of file
-        file_to_turn_into_json argument = the file to be stored as a json file'''
-        with open(path, 'w') as fp:
-            json.dump(file_to_turn_into_json, fp)
-    
-    @staticmethod
-    def crypto_data_UUID_list_dictionary(   record_list:list, 
-                                            path:str):
-        Dictionary = CMCScraper.UUID_dictionary(record_list)
-        CMCScraper.turn_dictionary_into_json_file(path, Dictionary)
-        return Dictionary
-
-    @staticmethod
-    def upload_file_to_s3(  file_path:str, 
-                            bucket:str, 
-                            object_name=None):
-            
-        """Upload a file to an S3 bucket
-
-        :param file_name: File to upload (directory)
-        :param bucket: Bucket to upload to (bucket name)
-        :param object_name: S3 object name, the name you want to give the file. If not specified then file_name is used
-        :return: True if file was uploaded, else False
-        """
-        file_name = str(ntpath.basename(file_path))
-        print(file_name) 
-        # If S3 object_name was not specified, use file_name
-        if object_name is None:
-            object_name = file_name
-        # Create instance of S3 
-        s3_client = boto3.client('s3')
-        #Get S3 contents and norrow down by Prefix=file_name
-        already_uploaded = s3_client.list_objects_v2(Bucket=bucket, Prefix=file_name)
-        #If 'Contents' exists in the search then there must be a match
-        if 'Contents' in already_uploaded:
-             print('File already in bucket')
-        else: s3_client.upload_file(file_path, bucket, object_name) 
-        
-    @staticmethod
-    def upload_folder_to_S3(folder_path:str, 
-                            bucket:str):
-        s3_client = boto3.client('s3')
-        for root,dirs,files in os.walk(folder_path):
-            for file in files:
-                file =str(file)
-                already_uploaded = s3_client.list_objects_v2(Bucket=bucket, Prefix=file)
-                print(already_uploaded)
-                if 'Contents' in already_uploaded:
-                    print('File already in bucket, no file uploaded') 
-                else: s3_client.upload_file(os.path.join(root, file), bucket, file)
-
-    @staticmethod
-    def upload_table_from_csv_to_RDS(path_to_csv:str, 
-                                    name_of_table:str):
-        
-        DATABASE_TYPE = 'postgresql'
-        DBAPI = 'psycopg2'
-        ENDPOINT = 'cmc-scraper-mo.c4ojkdkakmcp.eu-west-2.rds.amazonaws.com' # Change it to your AWS endpoint
-        USER = 'postgres'
-        PASSWORD = 'ABC123!!'
-        PORT = 5432
-        DATABASE = 'postgres'
-        engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}")
-        engine.connect()
-        #import the .csv which is stored and convert back to a dataframe
-        dataframe = pd.read_csv(path_to_csv)
-        print(dataframe.head(10))
-        dataframe.to_sql(name_of_table, engine)
-
-    @staticmethod
-    def get_RDS_to_dataframe(name_of_table=str):
-        DATABASE_TYPE = 'postgresql'
-        DBAPI = 'psycopg2'
-        ENDPOINT = 'cmc-scraper-mo.c4ojkdkakmcp.eu-west-2.rds.amazonaws.com' # Change it to your AWS endpoint
-        USER = 'postgres'
-        PASSWORD = 'ABC123!!'
-        PORT = 5432
-        DATABASE = 'postgres'
-        engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}")
-        engine.connect()
-        return pd.read_sql_table(name_of_table, engine)
-   
-    @staticmethod
-    def get_all_available_webpages():
-        #define date variables of first day of records and today's date to test all range of dates 
-        first_day_on_record = date(2013, 4, 28)
-        today = date.today()
-        #turn into strings to allow input to the method
-        first_day_on_record = str(first_day_on_record)
-        today = str(today)
-        #run the final method create_url_list_final
-        all_available_records = CMCScraper.create_url_list_final(first_day_on_record, today, 1, 'https://coinmarketcap.com/historical/')
-        df = pd.DataFrame(all_available_records, columns=["source_url"])
-        return df
-
-    @staticmethod
-    def compare_dataframes( dataframe1, 
-                            dataframe2):
-        set_for_differences = set(dataframe1['source_url']).difference(set(dataframe2['source_url']))
-        #return the list which has been reordered into dates: this is now the url list for rescraping
-        return list(sorted(set_for_differences))    
-    
-    @staticmethod
-    def compare_CSVs(   path_to_CSV1:str, 
-                        path_to_CSV2:str):
-        #Convert csv1 into df
-        df1 = pd.read_csv(path_to_CSV1)
-        #Convert csv2 into df
-        df2 = pd.read_csv(path_to_CSV2)
-        #Convert dfs to sets and then compare for differences
-        into_dataframes = CMCScraper.compare_dataframes(df1, df2)
-        return(into_dataframes)
    
     def upload_data_to_pre_existing_RDS(self, 
                                         sql_table_name:str):
@@ -597,12 +431,12 @@ class CMCScraper:
 
 if __name__ =="__main__":
     yolo = CMCScraper()
-    all_urls = yolo.create_url_list_final('04-28-2013', '05-23-2013', 1, 'https://coinmarketcap.com/historical/')
+    all_urls = yolo.create_url_list_final('04-28-2013', '05-05-2013', 1, 'https://coinmarketcap.com/historical/')
     all_data = yolo.get_crypto(all_urls, len(all_urls))
-    dataframe = yolo.create_crypto_dataframe(all_data)
+    dataframe = yolo.create_dataframe(all_data, "ID", "source_url", "Rank", "Name", "Market Capitalisation", "Price", "Circulating Supply", "Ticker", "24 h change")
     print(dataframe)
-    #csv = yolo.save_dataframe_locally(dataframe, r"C:\Users\marko\DS Projects\Data\scrape_up_to_23-09-22.csv")
-    #dictionary = yolo.crypto_data_UUID_list_dictionary(all_data, r"C:\Users\marko\DS Projects\Data\scrape_up_to_23-09-22.json")
+    csv = yolo.save_dataframe_locally(dataframe, r"C:\Users\marko\DS Projects\Data\test_restructure.csv")
+    dictionary = yolo.crypto_data_UUID_list_dictionary(all_data, r"C:\Users\marko\DS Projects\Data\test_restructure.json")
     #yolo.upload_data_to_pre_existing_RDS('crypto_datatable')
     #new_RDS = yolo.get_RDS_to_dataframe('crypto_datatable')
     #print(new_RDS)
